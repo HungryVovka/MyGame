1ab66e15ab07b3ead8131702ca221b52a22e81b0
Vg <nother@MacBook-Pro-Vg.local>
2023-06-09 21:04:52 +0300

    tool sounds and videos config impl

---
+++ addons/DialogHelperTool/VideoConfigItem/VideoStreamPlayer.gd
@@ -0,0 +1,27 @@
+@tool
+extends VideoStreamPlayer
+
+var image = null
+
+@onready
+var bgTexture = $"../PanelContainer/TextureRect"
+	
+func reset_image():
+	image = null
+	
+func _process(_delta):
+	if is_playing() && stream_position && !image:
+		store_first_frame()
+		set_process(false)
+
+func _on_finished():
+	play()
+
+func store_first_frame():
+	var newImage = self.get_video_texture()
+	if newImage:
+		var texture = ImageTexture.create_from_image(newImage.get_image())
+		if texture:
+			bgTexture.texture = texture
+			image = newImage
+			stop()
---
+++ addons/DialogHelperTool/VideosMarginContainer.gd
@@ -0,0 +1,19 @@
+@tool
+extends MarginContainer
+
+signal dropped_data(paths)
+
+func _can_drop_data(_at_position: Vector2, data: Variant):
+	return data.type == "files" && validate(data.files)
+	
+func validate(array):
+	var allowed_ext = ["ogv"]
+	for i in array:
+		var s: String = i
+		if !allowed_ext.has(s.get_extension()):
+			return false
+	return true
+		
+func _drop_data(_at_position, data):
+	dropped_data.emit(data.files)
+	
